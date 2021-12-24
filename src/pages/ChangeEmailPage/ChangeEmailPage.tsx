669516338873ff771f93e2d1145c1d8cc0a50379
nicolae.topala <nicolae.topala13@gmail.com>
2021-12-24 17:37:08 +0300

    added change password and email pages

---
+++ src/pages/ChangeEmailPage/ChangeEmailPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './ChangeEmailPage.scss';
+
+export const ChangeEmailPage = (): React.ReactElement => {
+  return <div>Change Email</div>;
+};
---
+++ src/pages/ChangePasswordPage/ChangePasswordPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './ChangePasswordPage.scss';
+
+export const ChangePasswordPage = (): React.ReactElement => {
+  return <div>Change Password</div>;
+};
