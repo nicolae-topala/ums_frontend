90bf6f5f6730fa08476be7abb440fa01105bdc9e
nicolae.topala <nicolae.topala13@gmail.com>
2022-01-02 02:22:59 +0300

    Improved Table atom

--- src/ui/atoms/Table/Table.tsx
+++ src/ui/atoms/Table/Table.tsx
@@ -12,6 +12,7 @@ interface Props {
   caption?: string;
   data?: dataType[];
   errorMessage?: string;
+  large?: boolean;
 }
 
 export const Table: React.FC<Props> = ({
@@ -19,24 +20,61 @@ export const Table: React.FC<Props> = ({
   caption = '',
   data,
   errorMessage,
+  large,
 }): React.ReactElement => {
   return (
     <>
-      <table className={`${className}`}>
-        <caption>{caption}</caption>
-        <tbody>
-          <tr>
-            <td>
-              <span className="name">GET name</span>
-            </td>
-            <td>
-              <span className="value">GET value</span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-      {errorMessage && <p>{errorMessage}</p>}
+      {large ? (
+        <>
+          <div className={`${large ? 'table-large' : ''}`}>
+            <table className={`${className}`}>
+              <caption>{caption}</caption>
+              <thead>
+                <tr>
+                  <th>GET name</th>
+                  <th>GET name</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <span className="value">GET value</span>
+                  </td>
+                  <td>
+                    <span className="value">GET value</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <span className="value">GET value</span>
+                  </td>
+                  <td>
+                    <span className="value">GET value</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            {errorMessage && <p>{errorMessage}</p>}
+          </div>
+        </>
+      ) : (
+        <>
+          <table className={`${className}`}>
+            <caption>{caption}</caption>
+            <tbody>
+              <tr>
+                <td>
+                  <span className="name">GET name</span>
+                </td>
+                <td>
+                  <span className="value">GET value</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          {errorMessage && <p>{errorMessage}</p>}
+        </>
+      )}
     </>
   );
 };
